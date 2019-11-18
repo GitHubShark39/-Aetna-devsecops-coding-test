c1912ab25bff847ecaa9561277fc8c46dcdac582
GitHubShark39 <blackshark37@gmail.com>
2019-11-17 20:18:45 -0500

    first commit

---
+++ php_array_merge.php
@@ -0,0 +1,39 @@
+
+
+<!DOCTYPE html>
+<html>
+<h4>PHP_array merge</h4>
+  <br>
+  <h5>  Royal C. Jackson Jr. blackshark37@gmail.com </h5>
+<h5> Date: 11.17.2019 </h5>
+<h5> https://github.com/aetnahealth/devsecops-coding-test </h5>
+<!--  Royal C. Jackson Jr. <blackshark37@gmail.com> -->
+<!-- Date: 11.17.2019 -->
+<!-- https://github.com/aetnahealth/devsecops-coding-test 
+
+<!-- php_array merge -->
+<!-- This file will be put into a dockerfile -->
+
+<!-- This is a comment -->
+<!-- This is a comment -->
+<!-- This is a comment -->
+<body>
+
+<?php
+    echo 'This is a Aetnahealth DevSecOps Coding Test'; // 
+  echo "<br>";
+    /* C:\0_aetnahealth_Repository\devsecops-coding-test */
+    echo '---------------------------';
+  echo "<br>";
+    echo 'php array merge Final Test'; 
+  echo "<br>";
+$a1=array("1, 2, 3, 6");
+$a2=array("7, 8, 9");
+print_r(array_merge($a1,$a2));
+
+
+
+?>
+
+</body>
+</html>
